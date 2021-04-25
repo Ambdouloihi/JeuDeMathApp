96cd2b7cf86aa18750600ccbee413d44f9f97280
Ambdouloihi <49631741+Ambdouloihi@users.noreply.github.com>
2021-04-25 19:14:14 +0300

    Ajout du quiz de calcul Mental

--- app/src/main/java/com/example/jeudemath/MainActivity.kt
+++ app/src/main/java/com/example/jeudemath/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.jeudemath
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.jeudemath.Categorie as ComExampleJeudemathCategorie
 
@@ -11,14 +14,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_layout)
+        title = "Callcul Mental"
 
         val btnJouer: Button = findViewById(R.id.btnJouer)
         btnJouer.setOnClickListener { goToCategoriePage() }
-
     }
-    private  fun goToCategoriePage(){
+
+    private fun goToCategoriePage() {
         val intent = Intent(this, ComExampleJeudemathCategorie::class.java)
-        // start your next activity
         startActivity(intent)
     }
+
+
 }
