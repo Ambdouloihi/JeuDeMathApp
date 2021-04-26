a7228a84a6c04fea4ff5b4df362b35b8a09bc273
Flatcheur <58169746+Flatcheur@users.noreply.github.com>
2021-04-26 15:23:01 +0400

    Add files via upload

---
+++ jeuDeMath/jeuDeMath/jeuDeMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  jeuDeMathApp.swift
+//  jeuDeMath
+//
+//  Created by mehong on 26/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct jeuDeMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
