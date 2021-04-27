4d27160862b4ed2759850c88240c3cb6b667e076
Ambdouloihi <49631741+Ambdouloihi@users.noreply.github.com>
2021-04-27 16:46:59 +0400

    git web
    ajout des contraintes d'éléments

---
+++ jeuDeMath/JeuDeMath/JeuDeMath/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  JeuDeMath
+//
+//  Created by etudiant on 4/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var btnJouer: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+}
+
