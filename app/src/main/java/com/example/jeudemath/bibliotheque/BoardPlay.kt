0f64ae9ff1f4f954c77d520448b349cb3a09f734
Ambdouloihi <49631741+Ambdouloihi@users.noreply.github.com>
2021-04-28 10:49:15 +0300

    arrangement des appel de fonction pour que le "remove" dans "collision" soit fait avant "invalidate"(permet de supprimer tous les elements sans en laisser un)

--- app/src/main/java/com/example/jeudemath/bibliotheque/BoardPlay.kt
+++ app/src/main/java/com/example/jeudemath/bibliotheque/BoardPlay.kt
@@ -10,21 +10,22 @@ import android.view.View
 
 class BoardPlay(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
     var listText = ArrayList<(leText)>()
-     val paint = Paint()
-    private var txtwidth=0
-    var mainAnswer=0
-    var mainI=0
-    var score=0
+    val paint = Paint()
+    private var txtwidth = 0
+    var mainAnswer = 0
+    var mainI = 0
+    var score = 0
 
 
     override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
         canvas.drawColor(Color.CYAN)
-        paint.color=Color.GRAY
+        paint.color = Color.GRAY
         canvas.drawText("$mainAnswer", (width / 2).toFloat(), (height / 2).toFloat(), paint)
-        canvas.drawText("Score : $score", (width).toFloat()-500, 100.0F, paint)
+        canvas.drawText("Score : $score", (width).toFloat() - 500, 100.0F, paint)
         updateDrawText(canvas)
         collision()
-        super.onDraw(canvas)
+        //remove()
         invalidate()
     }
 
@@ -42,7 +43,7 @@ class BoardPlay(context: Context?, attrs: AttributeSet?) : View(context, attrs) 
 
     init {
         paint.textSize = 100F
-        val txt="1 x 1"
+        val txt = "1 x 1"
         var bound = Rect()
         paint.getTextBounds(txt, 0, txt.length, bound);
         txtwidth = bound.width()
@@ -54,28 +55,30 @@ class BoardPlay(context: Context?, attrs: AttributeSet?) : View(context, attrs) 
         var txt: leText
         for (i in 1..10) {
             cx = randomNumber(0, width)
-            cy = randomNumber(-(height/2), height / 2)
+            cy = randomNumber(-(height / 2), height / 2)
             txt = leText(cx, cy, paint)
             listText.add(txt)
             if ((width - txt.twidth) < txt.x)
                 txt.x -= txt.twidth
 
         }
-        mainAnswer=listText[mainI].calcAnswer
+        mainAnswer = listText[mainI].calcAnswer
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-       for (text in listText) {
+        for (text in listText) {
             if (event != null) {
                 if (text.isTouch(event.x.toInt(), event.y.toInt())) {
                     if (text.color == Color.RED)
                         text.color = Color.GRAY
                     else
                         text.color = Color.RED
-                    if (text.calcAnswer==mainAnswer)  {
+                    if (text.calcAnswer == mainAnswer) {
                         text.color = Color.BLUE
-                        if ((mainI+1)<listText.size){ mainI++}
-                        mainAnswer=listText[mainI].calcAnswer
+                        if ((mainI + 1) < listText.size) {
+                            mainI++
+                        }
+                        mainAnswer = listText[mainI].calcAnswer
                         score++
                     } else score--
                 }
@@ -92,33 +95,33 @@ class BoardPlay(context: Context?, attrs: AttributeSet?) : View(context, attrs) 
         }
     }
 
-    fun collision(){
+    fun collision() {
         for (i in listText.lastIndex downTo 0) {
-          for (j in i - 1 downTo 0){
-              if(listText[i].isCollide(listText[j])){
-                  listText[i].y-=(listText[i].theight)
-              }
-              //remove
-              if (height  < listText[i].y) listText.removeAt(i)
-          }
-
+            for (j in i - 1 downTo 0) {
+                if (listText[i].isCollide(listText[j])) {
+                    listText[i].y -= (listText[i].theight)
+                }
+            }
+            if (height < listText[i].y) listText.removeAt(i)
         }
     }
+
     class leText(var x: Int, var y: Int, paint: Paint) {
         var color: Int = Color.GRAY
         var txt: String = Calcul().toString()
         var twidth = 0
         var theight = 0
         var bound = Rect()
-        var calcAnswer=0
+        var calcAnswer = 0
+
         init {
             paint.getTextBounds(txt, 0, txt.length, bound);
             twidth = bound.width()
             theight = bound.height()
 
-            var calc=Calcul()
-            txt= calc.toString()
-            calcAnswer=calc.answer
+            var calc = Calcul()
+            txt = calc.toString()
+            calcAnswer = calc.answer
         }
 
         fun move() {
@@ -127,7 +130,7 @@ class BoardPlay(context: Context?, attrs: AttributeSet?) : View(context, attrs) 
 
         fun draw(canvas: Canvas, paint: Paint) {
             paint.color = color
-            canvas.drawText(txt+"", x.toFloat(), y.toFloat(), paint)
+            canvas.drawText(txt + "", x.toFloat(), y.toFloat(), paint)
             move()
         }
 
@@ -135,8 +138,9 @@ class BoardPlay(context: Context?, attrs: AttributeSet?) : View(context, attrs) 
             return (x < x1) && (x1 < (x + twidth)) &&
                    ((y - theight) < y1) && (y1 < y)
         }
-        fun isCollide(txt:leText): Boolean {
-            return (txt.x < (x + twidth)) && (x < (txt.x+txt.twidth)) &&
+
+        fun isCollide(txt: leText): Boolean {
+            return (txt.x < (x + twidth)) && (x < (txt.x + txt.twidth)) &&
                    ((txt.y - txt.theight) < y) && ((y - theight) < txt.y)
         }
 
